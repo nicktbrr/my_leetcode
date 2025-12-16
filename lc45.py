937c6cfe213075df29881acb09d91e89770ee164
nick <nickbarsi@gmail.com>
2025-12-16 14:13:19 -0500

    more lc

---
+++ lc45.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        current = 0
+        farthest = 0
+        jumps = 0
+        for i in range(n - 1):
+            farthest = max(farthest, nums[i] + i)
+            if i == current:
+                jumps += 1
+                current = farthest
+        return jumps
+
+
+
+nums = [2,3,0,1,4]
+res = Solution().jump(nums)
+print(res)
