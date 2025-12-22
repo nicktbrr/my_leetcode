13a516e1395cf34cf9489ea85ade8a4f2e191747
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-12-21 16:07:38 -0800

    more lc

---
+++ lc15.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue
+            l,r = i + 1, len(nums) - 1
+            while l < r:
+                curr_sum = nums[i] + nums[l] + nums[r]
+                if curr_sum == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                elif curr_sum > 0:
+                    r -= 1
+                else:
+                    l += 1
+        return res
+
+
+
+i = [0,0,0,0]
+# i = [-1,0,1,2,-1,-4]
+print(Solution().threeSum(i))
