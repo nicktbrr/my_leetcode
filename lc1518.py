0034b97e0a5e7f34af080f2b4567801d2f62b25a
nick <nickbarsi@gmail.com>
2026-01-16 17:39:36 -0500

    more lc

---
+++ lc1518.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        res = 0
+        while numBottles >= numExchange:
+            res += numExchange
+            numBottles -= numExchange
+            numBottles += 1
+        return res + numBottles
